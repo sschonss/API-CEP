cce0f2587f8519e7eed3fd277316b0dbe405426b
schons <82298137+sschonss@users.noreply.github.com>
2022-04-26 11:09:20 -0300

    start-project

---
+++ index.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8">
+<h1>Pesquisar Endereço</h1>
+<form action="endereco.php" method="post">
+    <input type="text" name="cep">
+    <button type="submit">Pesquisar Endereço</button>
+</form>
+
